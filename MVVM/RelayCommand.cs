6dec0695aae4f73734cf30fa0cdfdf6d6e4dba6d
WaveZodium <wavezodium@gmail.com>
2025-10-20 20:48:39 +0200

    Make RelayCommand null-safe and add RaiseCanExecuteChanged.

--- MVVM/RelayCommand.cs
+++ MVVM/RelayCommand.cs
@@ -2,32 +2,35 @@
 
 namespace CookMaster.MVVM;
 
-class RelayCommand : ICommand {
+public class RelayCommand : ICommand {
     // Field to hold references to methods that define what to do (Execute)
-    private Action<object> execute;
+    private readonly Action<object?> _execute;
 
     // Checks if the command can be executed
-    private Func<object, bool> canExecute;
+    private readonly Func<object?, bool>? _canExecute;
 
-    // Event that signals when the command's ability to execute has changed
+    // Event that signals when the command's ability to _execute has changed
     public event EventHandler? CanExecuteChanged {
         add { CommandManager.RequerySuggested += value; }
         remove { CommandManager.RequerySuggested -= value; }
     }
 
     // Constructor that takes methods for Execute and CanExecute
-    public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null) {
-        this.execute = execute;
-        this.canExecute = canExecute;
+    public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null) {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
     }
 
     // Determines whether the command can be executed or not
     public bool CanExecute(object? parameter) {
-        return canExecute == null || canExecute(parameter);
+        return _canExecute == null || _canExecute(parameter);
     }
 
-    // Runs the logic assigned via the execute method
+    // Runs the logic assigned via the _execute method
     public void Execute(object? parameter) {
-        execute(parameter);
+        _execute(parameter);
     }
+
+    // Convenience to trigger re-evaluation of CanExecute from viewmodels
+    public void RaiseCanExecuteChanged() => CommandManager.InvalidateRequerySuggested();
 }
