724a0e667579f7393437270b3193de9770cc3021
WaveZodium <wavezodium@gmail.com>
2025-10-20 19:23:22 +0200

    Format documents to my preference.

--- Views/RecipeDetailWindow.xaml.cs
+++ Views/RecipeDetailWindow.xaml.cs
@@ -1,27 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace CookMaster.Views
-{
-    /// <summary>
-    /// Interaction logic for RecipeDetailWindow.xaml
-    /// </summary>
-    public partial class RecipeDetailWindow : Window
-    {
-        public RecipeDetailWindow()
-        {
-            InitializeComponent();
-        }
+namespace CookMaster.Views;
+
+/// <summary>
+/// Interaction logic for RecipeDetailWindow.xaml
+/// </summary>
+public partial class RecipeDetailWindow : Window {
+    public RecipeDetailWindow() {
+        InitializeComponent();
     }
 }
