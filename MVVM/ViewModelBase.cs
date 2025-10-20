219f4b7df59ab62d56a0a2aca9698d330ee7d62c
WaveZodium <wavezodium@gmail.com>
2025-10-20 20:31:37 +0200

    Add convenience setter to ViewModelBase.

--- MVVM/ViewModelBase.cs
+++ MVVM/ViewModelBase.cs
@@ -3,12 +3,20 @@ using System.Runtime.CompilerServices;
 
 namespace CookMaster.MVVM;
 
-class ViewModelBase : INotifyPropertyChanged {
+public class ViewModelBase : INotifyPropertyChanged {
     // INotifyPropertyChanged implementation
     public event PropertyChangedEventHandler? PropertyChanged;
 
     // Method to raise the PropertyChanged event
     public void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    // Convenience setter that raises PropertyChanged only when value changes
+    protected bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null) {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
     }
 }
