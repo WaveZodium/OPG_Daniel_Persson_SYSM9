dadb6de18cc80122b84d3efc63a82a054e9df3a1
WaveZodium <23120507+WaveZodium@users.noreply.github.com>
2025-10-22 09:09:57 +0200

    Add content in Recipe and create RecipeCategory enum.

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -1,4 +1,18 @@
 ﻿namespace CookMaster.Models;
 
 public class Recipe {
+    private Guid Id { get; init; } = Guid.NewGuid();
+    private string Title { get; set; } = string.Empty;
+    private string Ingredients { get; set; } = string.Empty;
+    private string Instructions { get; set; } = string.Empty;
+    private RecipeCategory Category { get; set; } = RecipeCategory.Other;
+    private DateTime Date { get; init; } = DateTime.UtcNow;
+    private User CreatedBy { get; init; } = new User();
+
+    public void EditRecipe() {
+    }
+
+    public void CopyRecipe() {
+    }
+
 }
---
+++ Models/RecipeCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookMaster.Models; 
+public enum RecipeCategory {
+    Appetizer,
+    MainCourse,
+    Dessert,
+    Beverage,
+    Salad,
+    Soup,
+    Snack,
+    Breakfast,
+    SideDish,
+    Other
+}
