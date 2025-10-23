db3d5fc49802e3d9359aedba1316192c2b28aa1d
WaveZodium <23120507+WaveZodium@users.noreply.github.com>
2025-10-23 10:53:04 +0200

    Change shared resources to implicit style instead of explicit. Fix delete button visibility in recipedetail based on user role.

--- ViewModels/RecipeDetailWindowViewModel.cs
+++ ViewModels/RecipeDetailWindowViewModel.cs
@@ -5,18 +5,29 @@ namespace CookMaster.ViewModels;
 
 public class RecipeDetailWindowViewModel : ViewModelBase {
     private readonly RecipeManager _recipeManager;
+    private readonly UserManager _userManager;
     private readonly IServiceProvider _services;
 
     // Close event for the view (parameter indicates success)
     public event Action<bool>? RequestClose;
 
+    // Added IsAdmin backing field and property so XAML can bind visibility
+    private bool _isAdmin;
+    public bool IsAdmin {
+        get => _isAdmin;
+        private set => Set(ref _isAdmin, value);
+    }
+
     public RelayCommand PerformSaveCommand { get; }
     public RelayCommand PerformDeleteCommand { get; }
     public RelayCommand PerformCloseCommand { get; }
 
-    public RecipeDetailWindowViewModel(RecipeManager recipeManager, IServiceProvider services) {
+    public RecipeDetailWindowViewModel(RecipeManager recipeManager, UserManager userManager, IServiceProvider services) {
         _recipeManager = recipeManager;
+        _userManager = userManager;
         _services = services;
+
+        IsAdmin = _userManager.IsAdmin;
 
         PerformSaveCommand = new RelayCommand(_ => PerformSave());
         PerformCloseCommand = new RelayCommand(_ => PerformClose());
